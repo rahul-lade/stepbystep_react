d52d0e3fc9c5cd3d5f4286387fcae7ef93a14d24
rahul-lade <rahullade935@gmail.com>
2026-02-15 12:28:07 +0530

    feat: replace display state with count state and implement conditional rendering based on count

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,13 +4,19 @@ import User from './User';
 
 
 function App() {
-  const [display,setdisplay] = useState(true);
- 
+  const [count,setcount] = useState(0);
+  
   return (
     <>
- <button onClick={()=>setdisplay(!display)}>hide</button>
- {display && <h1><User/></h1> }
-  {display && <h1>we are learning state</h1>}
+    <h1>{count}</h1>
+ <button onClick={()=>setcount(count+1)}>increase</button>
+ {count==0?<h1>condition 0</h1>
+ :count==1? <h1>condition 1</h1> 
+ :count==2? <h1>condition 2</h1> 
+ :count==3? <h1>condition 3</h1> 
+ :count==4? <h1>condition 4</h1> 
+ :count==5? <h1>condition 5</h1>
+ : <h1>other conditions</h1> }
 
      </>
   )
