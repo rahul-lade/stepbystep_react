16bde352e018457662a829569d8bb91c3f57a172
rahul-lade <rahullade935@gmail.com>
2026-02-17 09:28:16 +0530

    style: format Map component for improved readability

--- my-react-app/src/map.jsx
+++ my-react-app/src/map.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Map = ({usrrrer}) => {
-  return (
-    <div>
-        <h1>{usrrrer.name}</h1>
-        <h1>{usrrrer.age}</h1>
-        <h1>{usrrrer.email}</h1>
-    </div>
-  )
+const Map = ({ usrrrer }) => {
+    return (
+        <div>
+            <h1>{usrrrer.name}</h1>
+            <h1>{usrrrer.age}</h1>
+            <h1>{usrrrer.email}  </h1>
+        </div>
+    )
 }
 
 export default Map
